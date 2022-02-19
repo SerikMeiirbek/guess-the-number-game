632c878a2bb35539a62d147ee008fe83c84dd20a
serik.meiirbek@gmail.com <serik.meiirbek@gmail.com>
2022-02-18 23:07:08 -0500

    add ConsoleNumberGuess class

--- console/src/main/java/academy/learnprogramming/console/Main.java
+++ console/src/main/java/academy/learnprogramming/console/Main.java
@@ -1,21 +1,23 @@
-package academy.learnprogramming;
+package academy.learnprogramming.console;
 
+import academy.learnprogramming.AppConfig;
+import academy.learnprogramming.MessageGenerator;
+import academy.learnprogramming.NumberGenerator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
 
     private static final Logger log = LoggerFactory.getLogger(Main.class);
 
-    private static final String CONFIG_LOCATION = "beans.xml";
-
     public static void main(String[] args) {
         log.info("Guess the number game");
 
         //create context (container)
-        ConfigurableApplicationContext context = new ClassPathXmlApplicationContext(CONFIG_LOCATION);
+        ConfigurableApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+         //= new ClassPathXmlApplicationContext(CONFIG_LOCATION); XML path app config
 
         // get number generator bean from context (container)
         NumberGenerator numberGenerator = context.getBean( NumberGenerator.class);
@@ -27,11 +29,16 @@ public class Main {
         log.info("number = {}", number);
 
         // get game bean from context (container)
-        Game game = context.getBean(Game.class);
+        //Game game = context.getBean(Game.class);
 
         //call reset method
         //game.reset();
 
+        // get message generator bean from context (container)
+        MessageGenerator messageGenerator = context.getBean(MessageGenerator.class);
+
+        log.info("getMainMessage = {}", messageGenerator.getMainMessage());
+        log.info("getResultMessage = {}", messageGenerator.getResultMessage());
 
         // close context (container)
         context.close();
